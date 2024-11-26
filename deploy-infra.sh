8fdb8d79106643e46b53ec3b119f68d05033f9bb
binh.tran <b.tran@inpsyde.com>
2024-11-26 22:11:22 +0700

    add auto deploy

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -47,5 +47,6 @@ aws cloudformation deploy \
     # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports \
-    --query "Exports[?Name=='InstanceEndpoint'].Value"
+    --profile awsbootstrap \
+    --query "Exports[?starts_with(Name,'InstanceEndpoint')].Value"
 fi
